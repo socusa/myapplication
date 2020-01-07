5e6d24208cfdf71a04dd2607c090309b60a2f503
Keith Lynn <klynn@southalabama.edu>
2020-01-07 05:37:28 +0000

    Commit

---
+++ app/src/main/java/com/example/lynn/myapplication/MyListener.java
@@ -0,0 +1,12 @@
+package com.example.lynn.myapplication;
+
+import android.view.View;
+
+import static com.example.lynn.myapplication.MainActivity.*;
+
+public class MyListener implements View.OnClickListener {
+    @Override
+    public void onClick(View view) {
+
+    }
+}
---
+++ app/src/main/java/com/example/lynn/myapplication/MyView.java
@@ -0,0 +1,12 @@
+package com.example.lynn.myapplication;
+
+import android.content.Context;
+import android.widget.RelativeLayout;
+
+import static com.example.lynn.myapplication.MainActivity.*;
+
+public class MyView extends RelativeLayout {
+    public MyView(Context context) {
+        super(context);
+    }
+}
